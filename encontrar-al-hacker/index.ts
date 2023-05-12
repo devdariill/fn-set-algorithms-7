b8712b653277cd65f73480af431efb996b4a85db
nATOr <gowluonade@gmail.com>
2023-05-11 23:37:24 -0500

    for( for( ) ) - Math.min_forMax - [ ].push

--- encontrar-al-hacker/index.ts
+++ encontrar-al-hacker/index.ts
@@ -1,6 +1,30 @@
 import type { Estudiante } from "./types";
 
+const PUNTAJES = {
+  A: 30,
+  B: 20,
+  C: 10,
+  D: 5,
+};
+
 export default function encontrarHackers(estudiantes: Estudiante[]): string[] {
-  // TODO: implementar
-  return [];
+  const hackers: string[] = [];
+  for (let [nombre, puntaje, calificaciones] of estudiantes) {
+    if (puntaje > 200) {
+      hackers.push(nombre);
+      continue;
+    }
+    let tPuntajeEstudiante = 0;
+    for (const calificacion of calificaciones) {
+      tPuntajeEstudiante += PUNTAJES[calificacion] ?? 0;
+    }
+    if (calificaciones.every((calificacion) => ["B", "A"].includes(calificacion))) {
+      tPuntajeEstudiante += 20;
+    }
+    tPuntajeEstudiante = Math.min(200, tPuntajeEstudiante);
+    if (tPuntajeEstudiante !== puntaje) {
+      hackers.push(nombre);
+    }
+  }
+  return hackers;
 }
