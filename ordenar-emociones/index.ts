5b91cbd8655fc1cd21986bc64387c146f98e42ed
nATOr <gowluonade@gmail.com>
2023-05-11 22:31:52 -0500

    0

---
+++ ordenar-emociones/index.ts
@@ -0,0 +1,13 @@
+import type { Emocion } from "./types";
+
+const PUNTAJE: Record<Emocion, number> = {
+  T_T: 1,
+  ":(": 2,
+  ":|": 3,
+  ":)": 4,
+  ":D": 5,
+};
+
+export default function ordenarEmociones(emociones: Emocion[], orden: boolean): Emocion[] {
+  return [];
+}
