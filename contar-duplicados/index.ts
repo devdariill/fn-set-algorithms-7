e8418af0616b8a5557dc47fba33f9bb2f3e041d5
nATOr <gowluonade@gmail.com>
2023-05-11 23:45:56 -0500

    Set-has-add-size

--- contar-duplicados/index.ts
+++ contar-duplicados/index.ts
@@ -1,4 +1,23 @@
 export default function contarDuplicados(texto: string): number {
-  // TODO: implementar
-  return 0;
+  texto = texto.toLowerCase();
+  const apariciones = new Set<string>();
+  const duplicados = new Set<string>();
+  for (const letra of texto) {
+    if (apariciones.has(letra)) duplicados.add(letra);
+    else {
+      apariciones.add(letra);
+    }
+  }
+  return duplicados.size;
 }
+
+// export default function contarDuplicados(texto: string): number {
+//   const mapa = texto.split("").reduce<Record<string, number>>((acumulador, letra) => {
+//     letra = letra.toLowerCase();
+//     return {
+//       ...acumulador,
+//       [letra]: (acumulador[letra] ?? 0) + 1,
+//     };
+//   }, {});
+//   return Object.values(mapa).filter((cantidad) => cantidad > 1).length;
+// }
