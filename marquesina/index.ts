54d9eaad4dde4e647f342153aa700c2d2d31b967
nATOr <gowluonade@gmail.com>
2023-05-11 22:48:43 -0500

    for-slice

--- marquesina/index.ts
+++ marquesina/index.ts
@@ -1,4 +1,10 @@
 export default function marquesina(texto: string): string[] {
+  let buffer: string[] = [];
+  // for (let letra of texto){
+  for (let index = 1; index <= texto.length; index++) {
+    buffer.push(texto.slice(index) + texto.slice(0, index));
+  }
+  console.log(buffer);
   // TODO: implementar
-  return [];
+  return buffer;
 }
